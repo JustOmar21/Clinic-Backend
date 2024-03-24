c87789f97741547fc03f5a288ff98929a3dbeaa3
justomar21 <tarekes68@gmail.com>
2024-03-24 04:31:57 +0200

    Added DTOs

---
+++ Clinic.Core/DTO/PatientAppointments.cs
@@ -0,0 +1,15 @@
+﻿using Clinic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Core.DTO
+{
+    public class PatientAppointments
+    {
+        public Patient Patient { get; set; }
+        public List<Appointement> Appointements { get; set; }
+    }
+}
