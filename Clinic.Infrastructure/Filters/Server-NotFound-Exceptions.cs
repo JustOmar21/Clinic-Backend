7dd3a5e5f58a69147ae5b6af2280002aef1a6d9c
justomar21 <tarekes68@gmail.com>
2024-03-26 10:01:21 +0200

    Added Filter Exception

---
+++ Clinic.Infrastructure/Filters/Server-NotFound-Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Clinic.Infrastructure.Filters
+{
+    internal class Server_NotFound_Exceptions
+    {
+    }
+}
