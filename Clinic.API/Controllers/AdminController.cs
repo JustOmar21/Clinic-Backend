5b7a85dc232ed48c5ee9e4ed74c02b2cecf6db98
justomar21 <tarekes68@gmail.com>
2024-03-27 06:35:59 +0200

    fully Implemented Admin and Doctor Controller

---
+++ Clinic.API/Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Clinic.Core.DTO;
+using Clinic.Core.Repos;
+using Clinic.Infrastructure.Filters;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.API.Controllers
+{
+    [Server_NotFound_Exceptions]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminRepo _adminRepo;
+        public AdminController(IAdminRepo adminRepo)
+        {
+            _adminRepo = adminRepo;
+        }
+        [HttpPatch("Account/Activate")]
+        public IActionResult ActivateAccount(AccountStatus account)
+        {
+            return StatusCode((int)_adminRepo.ActivateAccount(account));
+        }
+        [HttpPatch("Account/Deactivate")]
+        public IActionResult DeactivateAccount(AccountStatus account)
+        {
+            return StatusCode((int)_adminRepo.DeactivateAccount(account));
+        }
+        [HttpPatch("Account/Banned")]
+        public IActionResult BanAccount(AccountStatus account)
+        {
+            return StatusCode((int)_adminRepo.BanAccount(account));
+        }
+        [HttpPatch("Account/RequestConfirm")]
+        public IActionResult ConfrimAccount(AccountStatus account)
+        {
+            return StatusCode((int)_adminRepo.ConfirmationRequest(account));
+        }
+        [HttpPost("Account/RequestKey")]
+        public IActionResult RequestKey(AccountStatus account)
+        {
+            return Ok(_adminRepo.GetKeypass(account))   ;
+        }
+    }
+}
---
+++ Clinic.API/Controllers/DoctorController.cs
@@ -0,0 +1,143 @@
+﻿using Clinic.Core.Models;
+using Clinic.Core.Repos;
+using Clinic.Infrastructure.Filters;
+using Clinic.Infrastructure.RepoImplemention;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinic.API.Controllers
+{
+    [Server_NotFound_Exceptions]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IDoctorRepo _doctorRepo;
+        public DoctorController(IDoctorRepo doctorRepo)
+        {
+            _doctorRepo = doctorRepo;
+        }
+        [HttpPost("Create")]
+        public IActionResult Add(Doctor doc)
+        {
+            return StatusCode((int)_doctorRepo.AddDoctor(doc));
+        }
+        [HttpPut("Update")]
+        public IActionResult Edit(Doctor doc)
+        {
+            return StatusCode((int)_doctorRepo.EditDoctor(doc));
+        }
+        [HttpDelete("Delete/{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return StatusCode((int)_doctorRepo.DeleteDoctor(id));
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetSingleDoctor(int id)
+        {
+            var patient = _doctorRepo.GetDoctor(id);
+            return Ok(patient);
+        }
+        [HttpGet("Details/{id:int}")]
+        public IActionResult GetSingleDoctorDetails(int id)
+        {
+            var patient = _doctorRepo.GetDoctorDetails(id);
+            return Ok(patient);
+        }
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&email={email:alpha}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&email={email:alpha}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}&specialityID={specialityID:int}")]
+        [HttpGet("page={pageNumber:int}&size={pageSize:int}")]
+        public IActionResult GetAllDoctors(int pageNumber, int pageSize, string Location = "", int specialityID = -1, string email = "")
+        {
+            var patient = _doctorRepo.GetAllDoctors(pageNumber, pageSize, Location, specialityID , email);
+            return Ok(patient);
+        }
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&email={email:alpha}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&Location={Location:alpha}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&email={email:alpha}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}&specialityID={specialityID:int}")]
+        [HttpGet("Details/page={pageNumber:int}&size={pageSize:int}")]
+        public IActionResult GetAllDoctorsDetails(int pageNumber, int pageSize, string Location = "", int specialityID = -1, string email = "")
+        {
+            var patient = _doctorRepo.GetAllDoctorsDetails(pageNumber, pageSize, Location, specialityID, email);
+            return Ok(patient);
+        }
+        [HttpGet("Count/&Location={Location:alpha}&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Count/&Location={Location:alpha}&email={email:alpha}")]
+        [HttpGet("Count/&Location={Location:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Count/&email={email:alpha}&specialityID={specialityID:int}")]
+        [HttpGet("Count/&Location={Location:alpha}")]
+        [HttpGet("Count/&email={email:alpha}")]
+        [HttpGet("Count/&specialityID={specialityID:int}")]
+        [HttpGet("Count")]
+        public IActionResult GetAllDoctorsCount(int pageNumber, int pageSize, string Location = "", int specialityID = -1, string email = "")
+        {
+            var patient = _doctorRepo.GetDoctorsCount(Location, specialityID, email);
+            return Ok(patient);
+        }
+        [HttpGet("Appointment/doctor={doctorID:int}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        [HttpGet("Appointment/doctor={doctorID:int}&date={date:datetime}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        public IActionResult GetAllAppointments(int doctorID, DateTime? date, int pageNumber = 1, int pageSize = 10)
+        {
+            if (date is not null)
+                date = date.Value.Date;
+            return Ok(_doctorRepo.GetAllAppointements(doctorID, date, pageNumber, pageSize));
+        }
+        [HttpGet("Appointment/Count/doctor={doctorID:int}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        [HttpGet("Appointment/Count/doctor={doctorID:int}&date={date:datetime}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        public IActionResult GetAllAppointmentsCount(int doctorID, DateTime? date)
+        {
+            if (date is not null)
+                date = date.Value.Date;
+            return Ok(_doctorRepo.GetAllAppointementsCount(doctorID, date));
+        }
+        [HttpGet("Appointment/doctor={docID:int}&patient={patientID:int}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        public IActionResult GetDocAppointments(int docID, int patientID, int pageNumber = 1, int pageSize = 10)
+        {
+            return Ok(_doctorRepo.GetPatientAppointements(docID, patientID, pageNumber, pageSize));
+        }
+        [HttpGet("Appointment/Count/doctor={docID:int}&patient={patientID:int}&pagenumber={pageNumber:int}&pagesize={pageSize:int}")]
+        public IActionResult GetDocAppointmentsCount(int docID, int patientID, int pageNumber = 1, int pageSize = 10)
+        {
+            return Ok(_doctorRepo.GetPatientAppointementsCount(docID, patientID));
+        }
+        [HttpPatch("Appointment/Cancel/{appID:int}")]
+        public IActionResult CancelAppointment(int appID)
+        {
+            return StatusCode((int)_doctorRepo.CancelAppointment(appID));
+        }
+        [HttpPatch("Appointment/Reject/{appID:int}")]
+        public IActionResult RejectAppointment(int appID)
+        {
+            return StatusCode((int)_doctorRepo.RejectAppointment(appID));
+        }
+        [HttpPatch("Appointment/Confirm/{appID:int}")]
+        public IActionResult ConfirmAppointment(int appID)
+        {
+            return StatusCode((int)_doctorRepo.ConfirmAppointment(appID));
+        }
+        [HttpGet("Schedule/{scheduleID:int}")]
+        public IActionResult GetSchedule(int scheduleID)
+        {
+            return Ok(_doctorRepo.GetSchedule(scheduleID));
+        }
+        [HttpGet("Schedule/Add")]
+        public IActionResult AddSchedule(Schedule schedule)
+        {
+            return StatusCode((int)_doctorRepo.AddSchedule(schedule));
+        }
+        [HttpGet("Schedule/Edit")]
+        public IActionResult EditSchedule(Schedule schedule)
+        {
+            return StatusCode((int)_doctorRepo.EditSchedule(schedule));
+        }
+    }
+}
