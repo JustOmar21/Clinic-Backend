4a7e8bc508182db1c28a928f9bad2c8e8f266dc0
justomar21 <tarekes68@gmail.com>
2024-03-31 16:58:29 +0200

    added changePassword DTO

---
+++ Clinic.Core/DTO/ChangePassword.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic.Core.DTO
+{
+    public class ChangePassword
+    {
+        public int userID { get; set; }
+        public string userRole { get; set; }
+        public string password { get; set; }
+        public string newPassword { get; set; }
+    }
+}
